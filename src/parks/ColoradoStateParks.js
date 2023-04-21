5433b96adbeae32ccab6e350c9a03742e3be05c7
kelvin901 <wawerukelvin2019@gmail.com>
2023-04-21 16:41:30 +0300

    Completed Assignment

---
+++ src/parks/ColoradoStateParks.js
@@ -0,0 +1,12 @@
+import React from "react";
+import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
+}
---
+++ src/parks/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import whoseHouse from "./Hogwarts";
+
+
+function Hogwarts() {
+  return (
+    <div className="Hogwarts">
+      "Harry. Did you put your name in the Goblet of Fire?"
+    </div>
+  );
+}
+
+function whoseHouse() {
+  console.log("HAGRID'S HOUSE!");
+}
+
+
+
+export default whoseHouse;
+
